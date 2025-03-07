4a96e51d6508bc4eb7ed6385afc2cac1910583e6
无尘 <2467337566@qq.com>
2025-03-07 18:07:59 +0800

    feat:添加Header

---
+++ src/layout/modules/Breadcrumb.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineOptions({
+  name: ''
+});
+
+import { useThemeStore } from '@/stores/modules/theme';
+const themeStore = useThemeStore();
+</script>
+
+<template>
+  <ElBreadcrumb v-if="themeStore.header.breadcrumb.visible">
+    <ElBreadcrumbItem :to="{ path: '/' }">homepage</ElBreadcrumbItem>
+    <ElBreadcrumbItem>
+      <ElDropdown>
+        <p>Dropdown List</p>
+        <template #dropdown>
+          <ElDropdownMenu>
+            <ElDropdownItem> Action 1 </ElDropdownItem>
+          </ElDropdownMenu>
+        </template>
+      </ElDropdown>
+    </ElBreadcrumbItem>
+  </ElBreadcrumb>
+</template>
+
+<style lang="scss" scoped></style>
--- src/layout/modules/Header.vue
+++ src/layout/modules/Header.vue
@@ -2,10 +2,54 @@
 defineOptions({
   name: ''
 });
+import { useThemeStore } from '@/stores/modules/theme';
+
+import Logo from '@/layout/modules/Logo.vue';
+import Breadcrumb from '@/layout/modules/Breadcrumb.vue';
+
+interface Props {
+  /** Whether to show the logo */
+  showLogo?: App.Global.HeaderProps['showLogo'];
+  /** Whether to show the menu toggler */
+  showMenuToggler?: App.Global.HeaderProps['showMenuToggler'];
+  /** Whether to show the menu */
+  showMenu?: App.Global.HeaderProps['showMenu'];
+}
+
+defineProps<Props>();
+
+const themeStore = useThemeStore();
 </script>
 
 <template>
-  <div>Header</div>
+  <div class="layout-Header">
+    <Logo v-if="showLogo" class="layout-Logo" :style="{ width: themeStore.sider.width + 'px' }" />
+    <div v-if="showMenu" id="__GLOBAL_HEADER_MENU__" class="layout-Header __GLOBAL_HEADER_MENU__"></div>
+    <div v-else class="layout-Header __GLOBAL_HEADER_MENU__">
+      <Breadcrumb class="layout-Breadcrumb" />
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-Header {
+  display: flex;
+  align-items: center;
+  height: 100%;
+  padding: 0 12px;
+}
+
+.layout-Logo {
+  height: 100%;
+}
+
+.__GLOBAL_HEADER_MENU__ {
+  height: 100%;
+  overflow: hidden;
+  flex: 1;
+
+  .layout-Breadcrumb {
+    margin-left: 12px;
+  }
+}
+</style>
