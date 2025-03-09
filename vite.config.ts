d32694bf265e9505f6692a068f6ea320a7ca1e72
cigarette <2467337566@qq.com>
2025-03-10 00:57:43 +0800

    feat:添加tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,14 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    tailwindcss(),
     vueDevTools(),
     AutoImport({
       imports: ['vue'],
